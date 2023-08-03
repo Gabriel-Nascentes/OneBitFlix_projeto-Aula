f41f8148976359215bf4137240c8750a554a8f3a
Gabriel Nascentes <gno.gabriel@gmail.com>
2023-08-03 16:47:24 -0300

    Preparando o projeto para trabalhar com Sequelize e efetuando a configuração.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { sequelize } from "./database/index";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor começou com sucesso na porta: ${PORT}`);
+app.listen(PORT, async () => {
+  await sequelize.authenticate().then(() => {
+    console.log("DB connection successfull.");
+  });
+  console.log(`Server started successfuly at port ${PORT}`);
 });
