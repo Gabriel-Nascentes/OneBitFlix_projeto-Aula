6fe10ab86c5f7ac7047fe896941bea1f18988a0b
Gabriel Nascentes <gno.gabriel@gmail.com>
2023-08-03 17:26:09 -0300

    Configuração inicial do AdminJS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from "express";
 import { sequelize } from "./database/index";
+import { adminJs, adminJsRouter } from "./adminjs/index";
 
 const app = express();
+
+app.use(express.static("public"));
+
+// app.use('caminho',rotas)
+app.use(adminJs.options.rootPath, adminJsRouter);
 
 const PORT = process.env.PORT || 3000;
 
